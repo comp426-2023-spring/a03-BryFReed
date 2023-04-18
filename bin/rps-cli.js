a95f861c2d1dd94c67d6e5272f269211ba9b1376
BryFReed <bfr7011@nyu.edu>
2023-04-17 22:13:58 -0400

    trying to fix string error

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -3,7 +3,7 @@
 import minimist from 'minimist';
 import { rps } from '../lib/rpsls.js';
 const args = minimist(process.argv.slice(2));
-const help = 'Usage: node-rps [SHOT]
+const help = `Usage: node-rps [SHOT]
 Play Rock Paper Scissors (RPS)
   -h, --help      display this help message and exit
   -r, --rules     display the rules and exit
@@ -11,11 +11,8 @@ Examples:
   node-rps        Return JSON with single player RPS result.
                   e.g. {"player":"rock"}
   node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                  e.g {"player":"rock","opponent":"scissors","result":"win"}}";
-const rules = "Rules for Rock Paper Scissors:
-    - Scissors CUTS Paper
-    - Paper COVERS Rock
-    - Rock CRUSHES Scissors';
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}}`;
+
 if (args.h || args.help) {
     console.log(help);
     process.exit(0);
